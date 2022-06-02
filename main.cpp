eac4af9bb6ef46c84f31effab618601a4b7dc700
Trần Hải Phước <93334312+tranhaiphuoc@users.noreply.github.com>
2022-06-02 10:13:43 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+
+using namespace std;
+
+//------------DEFINE_A_NODE-------------//
+struct Node {
+	int data;
+	struct Node *pNext;
+};
+
+//------------DEFINE_A_EMPTY_LIST-------------//
+struct List {
+	Node *pHead;
+	Node *pTail;
+};
+
+void init(List &l) {
+	l.pHead = l.pTail = NULL;
+};
+
+//------------CREATE_A_NEW_NODE-------------//
+Node* createNode() {
+	printf("Nhap gia tri: ");
+	int data;
+	scanf("%d", &data);
+
+	// Append data to new node
+	Node *p = new Node;
+	p->data = data;
+	p->pNext = NULL;
+	return p;
+}
+
+//------------CHECK_THE_LIST_IS_EMPTY-------------//
+void checkListIsEmpty(List &l, Node *p) {
+	if (l.pHead == NULL && l.pTail == NULL) {
+		l.pHead = l.pTail = p;
+	}
+}
+
+//------------APPEND_TO_THE_HEAD_OR_TAIL-------------//
+void toHead(List &l, Node *p) {
+	p->pNext = l.pHead;
+	l.pHead = p;
+}
+
+void toTail(List &l, Node *p) {
+	l.pTail->pNext = p;
+	l.pTail = p;
+}
+//------------DELETE_A_NODE-------------//
+
+
+//------------MAIN-------------//
+int main() {
+	// Make a List
+	List l;
+	init(l);
+	
+	// Get 'num' for loop
+	printf("Nhap so lan can nhap: ");
+	int num;
+	scanf("%d", &num);
+	
+	// Create and append new Node for 'num' times
+	int i = 0;
+	while (i < num) {
+		Node *p = createNode();
+		checkListIsEmpty(l, p);
+		toTail(l, p);
+		i++;
+	}
+	
+	// Print data in each Node of the List
+	for (Node *q=l.pHead; q!=NULL; q=q->pNext)
+		printf("%d ", q->data);
+		
+	
+	getchar();
+	return 0;
+}
